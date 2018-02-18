e8cb52645fe61fbe5286deb1e2a9813be8dce9de
xharmagne <xharmagne@gmail.com>
2018-02-18 14:51:32 +1100

    Explode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import AFRAME from "aframe";
-// import * as particleSystem from "aframe-particle-system-component";
+import * as particleSystem from "aframe-particle-system-component";
 
 AFRAME.registerComponent("circle-of-spheres", {
   init: function() {
@@ -17,6 +17,7 @@ AFRAME.registerComponent("circle-of-spheres", {
       entityEl.setAttribute("color", "#aaaacc");
       entityEl.setAttribute("shadow", "");
       entityEl.setAttribute("cursor-listener", "");
+      entityEl.setAttribute("class", "clickable");
       sceneEl.appendChild(entityEl);
     }
   }
@@ -24,46 +25,36 @@ AFRAME.registerComponent("circle-of-spheres", {
 
 AFRAME.registerComponent("cursor-listener", {
   init: function() {
-    this.el.addEventListener("fusing", function(evt) {
-      console.log("fusing");
+    const el = this.el;
+
+    // el.addEventListener("fusing", function(evt) {
+    //   console.log("fusing");
+    // });
+
+    el.addEventListener("click", function() {
+      explode(el.components.position.data);
+      el.parentNode.removeChild(el);
     });
   }
 });
 
-function addEntities() {}
+function explode({ x, y, z }) {
+  const sceneEl = document.querySelector("a-scene");
+  const entityEl = document.createElement("a-entity");
+  entityEl.setAttribute("position", `${x} ${y} ${z}`);
+  entityEl.setAttribute(
+    "particle-system",
+    `duration: .2;
+    maxAge: .3;
+    positionSpread: 0 0 0;
+    size: 0.1;
+    accelerationValue: 1 1 1;
+    accelerationSpread: 0 0 0;
+    velocityValue: 1 1 1;
+    velocitySpread: 0 0 0;
+    color: #ffffff;
+    particleCount: 200`
+  );
 
-// function addSnow() {
-//   const sceneEl = document.querySelector("a-scene");
-//   const entityEl = document.createElement("a-entity");
-//   entityEl.setAttribute("position", `0 0 0`);
-//   entityEl.setAttribute("particle-system", "preset: snow");
-
-//   sceneEl.appendChild(entityEl);
-// }
-
-// function addExplosion() {
-//   const sceneEl = document.querySelector("a-scene");
-//   const entityEl = document.createElement("a-entity");
-//   entityEl.setAttribute("position", `0 2 -5`);
-//   entityEl.setAttribute(
-//     "particle-system",
-//     `duration: .2;
-//     maxAge: .3;
-//     positionSpread: 0 0 0;
-//     size: 0.1;
-//     accelerationValue: 1 1 1;
-//     accelerationSpread: 0 0 0;
-//     velocityValue: 1 1 1;
-//     velocitySpread: 0 0 0;
-//     color: #ffffff;
-//     particleCount: 200`
-//   );
-
-//   sceneEl.appendChild(entityEl);
-// }
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  // addEntities();
-  // addExplosion();
-  // // addSnow();
-});
+  sceneEl.appendChild(entityEl);
+}
